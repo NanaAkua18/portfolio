7934fff99dfa6b1b501e38d5ba7c56e9eeb5e2e0
Nana Akua Saa Saifah <nanasaasaifah@gmail.com>
2024-11-06 11:39:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 120);
 });
 
-let menu = document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
     navlist.classList.toggle('active');
 }
 
-menu.onclick = () => {
-    menu.classList.remove('bx-x');
+menuIcon.onclick = () => {
+    menuIcon.classList.remove('bx-x');
     navlist.classList.remove('active');
 }
 
@@ -27,51 +27,3 @@ menu.onclick = () => {
 
 
 
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
-
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
-
-// Typewriter Effect
-
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "YOUTUBER"
-]
-
-let speed  =100;
-const textElements = document.querySelector(".typewriter-text");
-
-let textIndex = 0;
-let charcterIndex = 0;
-
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
-    }
-}
-
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
-
-window.onload = typeWriter
